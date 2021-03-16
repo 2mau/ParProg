7482bc288721ae231564edb96ea42d11e7c56b91
martin auer <csaw4907@student.uibk.ac.at>
2021-03-16 12:04:00 +0100

    fix: add stuff

--- assignment02_Sexten/mandelbrot/mandelbrot.c
+++ assignment02_Sexten/mandelbrot/mandelbrot.c
@@ -13,7 +13,31 @@
 #define MAX_ITER 10000
 
 void calc_mandelbrot(uint8_t image[Y][X]) {
-	// TODO: Write your code here!
+	struct timeval start, end;
+	gettimeofday(&start, NULL);
+
+	for(int Py = 0; Py < Y; Py++) {
+		for(int Px = 0; Px < X; Px++) {
+			int x = 0.0;
+			int y = 0.0;
+			double cx = ((float)Px / X) * 3.5 - 2.5;
+			double cy = ((float)Py / Y) * 2 - 1;
+
+			int iteration = 0;
+			while(x * x + y * y <= 2 * 2 && iteration < MAX_ITER) {
+				double x_tmp = x * x - y * y + cx;
+				double y = 2 * x * y + cy;
+				double x = x_tmp;
+				double iteration = iteration + 1;
+			}
+
+			double norm_iteration = (float)iteration / MAX_ITER * 255;
+			image[Px][Py] = norm_iteration;
+		}
+	}
+	gettimeofday(&end, NULL);
+	double elapsed_time = (end.tv_sec + end.tv_usec * 1e-6) - (start.tv_sec + start.tv_usec * 1e-6);
+	printf("Mandelbrot set calculation took: %f seconds.\n", elapsed_time);
 }
 
 int main() {
