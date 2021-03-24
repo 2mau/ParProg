fad0711506e54e36179a7ed7bf11944d4aab3ba3
Fabian Amhof <cb761125@login.intern.lcc2.uibk.ac.at>
2021-03-24 12:19:19 +0100

    Ex1

---
+++ assignment03/exercise1/run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Executes job in the queue "lva.q" unless you have special requirements.
+#$ -q lva.q
+
+# Changes to the current working directory before performing any further action
+#$ -cwd
+
+# Name of your job. Unless you use the -o and -e options, output will
+# go to a unique file name.ojob_id for each job.
+#$ -N Week1_Ex2
+
+# Redirect output stream to this file.
+#$ -o output.dat
+
+# Join the error stream to the output stream.
+#$ -j yes
+
+# Parallel environment for using OpenMP, allocates 8 cores on a single node
+#$ -pe openmp 8
+
+/usr/bin/time -v ./montecarlo_atomic
