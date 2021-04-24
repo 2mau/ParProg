7dccde609a2f2d822461d2ecf7c1fefbad8579f6
LV 703078 PS Parallele Programmierung <cb761013@login.intern.lcc2.uibk.ac.at>
2021-04-24 13:34:54 +0200

    assignment05

---
+++ assignment05_Sexten/exercise2/run2.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Executes job in the queue "lva.q" unless you have special requirements.
+#$ -q lva.q
+
+# Changes to the current working directory before performing any further action
+#$ -cwd
+
+# Name of your job. Unless you use the -o and -e options, output will
+# go to a unique file name.ojob_id for each job.
+#$ -N my_test_job
+
+# Redirect output stream to this file.
+#$ -o bench.dat
+
+# Join the error stream to the output stream.
+#$ -j yes
+
+# Parallel environment for using OpenMP, allocates 8 cores on a single node
+#$ -pe openmp 8
+
+# Use gcc 8.2.0 as the default gcc
+module load gcc/8.2.0
+
+# Set up any environment variables
+# export ENVIRONMENT_VARIABLE=foobar
+
+# ./path/to/application <command> <line> <arguments>
+
+make clean
+make
+
+for place in threads cores sockets
+do
+    for n in 1 4
+    do
+        for proc_bind in 'true' 'false' master close spread
+        do
+            for i in $(seq 1 5)
+            do
+                qsub \
+                    -N a4_e1_${n}-${place}_${proc_bind}_${i} \
+                    # -o stuff/lcc2_logs/${n}-${place}_${proc_bind}_${i}.log \
+                    -pe openmp 4 \
+                    -v OMP_NUM_THREADS=$n \
+                    -v OMP_PLACES=$place \
+                    -v OMP_PROC_BIND=$proc_bind \
+                    heat_stencil_2D.c
+            done
+        done
+    done
+done
+
+make clean
